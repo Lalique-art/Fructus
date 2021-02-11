d28b4c4bc5cd71a42ad19c5cffba03aa5a8802b8
Itunu Raimi <chavitunu@gmail.com>
2021-02-11 15:29:25 +0100

    designed fruit card view

--- Fructus/View/FruitCardView.swift
+++ Fructus/View/FruitCardView.swift
@@ -8,13 +8,49 @@
 import SwiftUI
 
 struct FruitCardView: View {
+//    MARK: - PROPERTIES
+    
+//    MARK: - BODY
+    
+//    MARK: - PREVIEW
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ZStack {
+            VStack(spacing: 20) {
+                // FRUIT: IMAGE
+                Image("blueberry")
+                    .resizable()
+                    .scaledToFit()
+                    .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 8, x: 6, y: 8)
+                
+                // FRUIT: TITLE
+                Text("BlueBerry")
+                    .foregroundColor(.white)
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 2, x: 2, y: 2)
+                
+                // FRUIT: HEADLINE
+                Text("Blueberries are sweet, nutritious and wildly popular fruits all over the world.")
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 16)
+                    .frame(maxWidth: 480)
+                
+                // BUTTON: START
+            }//: VSTACK
+        } //: ZSTACK
+        .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, minHeight: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/,maxHeight: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+        .background(LinearGradient(gradient: Gradient(colors: [Color("ColorBlueberryLight"), Color("ColorBlueberryDark")]), startPoint: .top, endPoint: .bottom))
+        .cornerRadius(20)
+        
     }
 }
 
 struct FruitCardView_Previews: PreviewProvider {
     static var previews: some View {
         FruitCardView()
+            .previewLayout(.fixed(width: 320, height: 640))
     }
 }
