c248fb10fcb8ddcbd45ebbe9faa95d7e1ede674d
Itunu Raimi <chavitunu@gmail.com>
2021-02-12 12:11:38 +0100

    added static data to fruit details screen

--- Fructus/App/FruitDetailView.swift
+++ Fructus/App/FruitDetailView.swift
@@ -14,7 +14,42 @@ struct FruitDetailView: View {
     
     //MARK: BODY
     var body: some View {
-        Text(fruit.title)
+        NavigationView {
+            ScrollView(.vertical, showsIndicators: true, content: {
+                VStack(alignment: .center, spacing: 20, content: {
+                    // HEADER
+                    
+                    VStack(alignment: .leading, spacing: 20, content: {
+                        // TITLE
+                        Text(fruit.title)
+                            .font(.largeTitle)
+                            .fontWeight(.heavy)
+                            .foregroundColor(fruit.gradientColors[1])
+                        
+                        // HEADLINE
+                        Text(fruit.headline)
+                            .font(.headline)
+                            .multilineTextAlignment(.leading)
+                        
+                        //NUTRIENTS
+                        
+                        //SUBHEADLINE
+                        Text("Learn more about \(fruit.title)".uppercased())
+                            .fontWeight(.bold)
+                            .foregroundColor(fruit.gradientColors[1])
+                        
+                        //DESCRIPTION
+                        Text(fruit.description)
+                            .multilineTextAlignment(.leading)
+                        // LINK
+                        
+                    }) //: VSTACK
+                    .padding(.horizontal, 20)
+                    .frame(maxWidth: 640, alignment: .center)
+                }) //: VSTACK
+            }) //: SCROLLVIEW
+        } //: NAVIGATION
+        
     }
 }
 
